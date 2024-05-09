2f8b1c506529bfc685855ec077721c75636d2a01
minzzjo <minxxjozz@gmail.com>
2024-05-10 00:49:57 +0900

    TODO - function create, delete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "../style/Todo.css";
-import { v4 as uuidv4 } from "uuid";
+import { nanoid } from "nanoid";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 import { FaTrashCan } from "react-icons/fa6";
 
 export default function Todo() {
+	let id = nanoid();
 	const [mode, setMode] = useState(false);
 	const onClickMode = () => {
 		console.log("mode switch!");
@@ -17,43 +18,35 @@ export default function Todo() {
 	};
 
 	const [done, setDone] = useState(false);
-	const onClickDone = () => {
-		console.log("clicked!");
-		if (done === false) {
-			setDone(true);
-		} else {
-			setDone(false);
-		}
+	const onClickDone = (e) => {
+		const targetId = e.target.name;
+		todoList.map((t) => {
+			if (t.id === targetId) {
+				setDone(!t.done);
+			}
+		});
 	};
 
-	const [show, setShow] = useState(false);
+	const [todoList, setTodoList] = useState([]);
+	const [todo, setTodo] = useState({ id: "", todo: "" });
 
-	const [id, setId] = useState();
-	const uuidFromUUIDV4 = () => {
-		const newID = uuidv4();
-		setId(newID);
-	};
-
-	const [item, setItem] = useState({ id: "", item: "" });
-	const [todo, setTodo] = useState([]);
-	const onChangeItem = (e) => {
-		const { name, value } = e.target;
-		uuidFromUUIDV4();
-		setItem({ ...item, id: id, [name]: value });
-		setShow(true);
+	const onChangeTodo = (e) => {
+		const { name, value, done } = e.target;
+		setTodo({ ...todo, id, [name]: value, done });
 	};
 
 	const onClickAdd = (e) => {
-		setTodo([...todo, item]);
 		e.preventDefault();
-		console.log("input value", item);
 		console.log(todo);
-		setItem({ id: "", item: "" });
+		setTodoList([...todoList, todo]);
+		console.log(todoList);
+		setTodo({ todo: "" });
 	};
 
-	// TODO: 삭제 기능 구현
-	const onClickDelete = () => {
-		console.log("delete");
+	const onClickDelete = (e) => {
+		const targetId = e.target.name;
+		const result = todoList.filter((todo) => todo.id !== targetId);
+		setTodoList(result);
 	};
 
 	return (
@@ -65,7 +58,6 @@ export default function Todo() {
 						{mode ? <MdDarkMode /> : <MdLightMode />}
 					</button>
 				</div>
-				{/* TODO: 버튼 눌렀을 때 상태별로 나눠서 보여주기 */}
 				<nav className='header__nav'>
 					<button className='header__nav__btn' id='all'>
 						All
@@ -82,37 +74,26 @@ export default function Todo() {
 			{/* Main - Lists */}
 			<section className='todo__lists'>
 				<ul>
-					{/* FIXME: map돌려서 붙기는 하는데 체크박스 상태가 전부 연결되어있음.. */}
-					{show && (
-						<>
-							{todo.map((list) => {
-								if (todo !== "" && todo.length !== 0) {
-									return (
-										<div className='todo__list__box' key={list.id}>
-											<div className='todo__list'>
-												<button className='todo__list__checkbtn' onClick={onClickDone}>
-													{done ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />}
-												</button>
-												<li className='todo__list__item'>{list.item}</li>
-											</div>
-											<button className='todo__delete__btn' onClick={onClickDelete}>
-												<FaTrashCan />
-											</button>
-										</div>
-									);
-								} else {
-									return "";
-								}
-							})}
-						</>
-					)}
+					{todoList.map((t) => (
+						<div key={t.id} className='todo__list'>
+							<button onClick={onClickDone} name={t.id} value={t.todo} done={t.done} className='todo__list__checkbtn'>
+								{/* {done ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />} */}
+								{done ? "✅" : "❎"}
+							</button>
+							<li>{t.todo}</li>
+							<button onClick={onClickDelete} name={t.id} value={t.todo} className='todo__delete__btn'>
+								{/* <FaTrashCan /> */}
+								del
+							</button>
+						</div>
+					))}
 				</ul>
 			</section>
 
 			{/* Footer - Add */}
 			<footer className='todo__add'>
 				<form className='todo__add__form' onSubmit={onClickAdd}>
-					<input type='text' className='todo__add__input' onChange={onChangeItem} name='item' value={item.item} id='item' placeholder='Add Todo' />
+					<input type='text' className='todo__add__input' onChange={onChangeTodo} name='todo' value={todo.todo} id='todo' placeholder='Add Todo' />
 					<button className='todo__add__btn'>Add</button>
 				</form>
 			</footer>
